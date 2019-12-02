91bec17cffec6de8c11e6e1a5224683d2fb5384d
Tyrik Gibbs <tyrikgibbs@outlook.com>
2019-12-02 03:38:22 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 # Add  code here!
 def prime?(integer)
-  if integer %2 != 0 && integer != 2
-    return false
-  else
+  if integer %2 !=0 && integer !=2 && integer < 1
     return true
+  else
+    return false
   end
 end
